f6ad930f1e2743bf0ee4c407550e9dca3fc673e6
Jiankun ZHAO <99773495+Ching-Yee-Chan@users.noreply.github.com>
2024-05-11 21:27:10 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,11 @@
 git clone https://github.com/VinAIResearch/blur-kernel-space-exploring bkse
 
+# Download VAE kernel for nonlinear deblur
+# If the command failed, it means that the link has been expired
+# In this case, please download from https://github.com/VinAIResearch/blur-kernel-space-exploring?tab=readme-ov-file#model-zoo on your own
 cd bkse/experiments/pretrained
 filename='GOPRO_wVAE.pth'
-fileid='1QMUY8mxUMgEJty2Gk7UY0WYmyyYRY7vS'
+fileid='1vRoDpIsrTRYZKsOMPNbPcMtFDpCT6Foy'
 wget --load-cookies /tmp/cookies.txt "https://drive.usercontent.google.com/download?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.usercontent.google.com/download?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
 
 cd ../../..
